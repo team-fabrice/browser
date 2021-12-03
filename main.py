c2d93ed40c42fe40de956d45c51b747b32b9c271
Wartapoo <mathisg@wp-corp.eu.org>
2021-12-03 06:07:49 +0100

    Removed useless header comments

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-# This is a sample Python script.
-
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import psycopg2
 import json
 import datetime
