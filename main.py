6713c7c733fb12aed0f937dc06ceb27380fd212d
Mattéo Decorsaire <matteo.decorsaire@etu.univ-grenoble-alpes.fr>
2021-12-03 04:59:45 +0100

    Working Argument

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-# This is a sample Python script.
-
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import psycopg2
+import json
+import datetime
 
-S = "dbname=nuit_info user=matteo password=dev"
-
+LIST_KEY= [ "revision_id",
+            "article_id",
+            "title",
+            "content",
+            "created_at",
+            "updated_at",
+            "modification_author",
+            "meta_class",
+            "meta_person_first_name", # Défini seulement si person
+            "meta_person_last_name", # Défini si person (nom du gars) ou event (nom du bateau)
+            "meta_person_birth",
+            "meta_person_death",
+            "meta_event_date", # Défini si event et l'event possède une date
+            "meta_location", # Défini si event et l'event possède une localisation
+            ]
 
 def init(S):
     conn = psycopg2.connect(S)
@@ -13,17 +24,51 @@ def init(S):
     return cur
 
 
-def recherche_name(name, year, cur):
-    cur.execute("""SELECT * FROM users WHERE name LIKE %s AND year=%s""", (name, year))
+#remplace le JSON issu du questionnaire
+def init_dico_cond():
+    return   {
+            #"title" : row[2],
+            "meta_person_first_name" : "Louis",
+            "meta_person_last_name" : "Bossu",
+            # "meta_event_date" : row[12],
+            # "meta_location" : row[13],
+    }
+
+def recherche_last_name(cur, name):
+    cur.execute("""SELECT * FROM article_rev WHERE meta_person_last_name LIKE %s""", (name,))
     rows = cur.fetchall()
+
+    List = []
     for row in rows:
-        print("ID = ", row[0], row[1], row[2])
-    return rows;
+        dico = {}
+        i=0
+        for key in LIST_KEY:
+            if type(row[i]) is datetime.date :
+                dico[key] = str(row[i].year)+"-"+str(row[i].month)+"-"+str(row[i].day)
+            else:
+                dico[key] = row[i]
+            i += 1
 
+        List.append(dico)
+    return List
 
+def filtre(var, filtre, list):
+    result = []
+    for row in list:
+        if row[var] == filtre :
+            result.append(row)
+    return result
 
+def recherche(cur, cond):
+    L = recherche_last_name(cur, cond["meta_person_last_name"])
+    for key in LIST_KEY:
+        if (key in cond) and key!= "meta_person_last_name":
+            L =filtre(key, cond[key], L)
+    return L;
+
+#générer le JSON de retour
 if __name__ == '__main__':
     cur = init(S)
-    name = input("Entrer votre nom :\n")
-    year = input("Entrer votre année :\n")
-    rows = recherche_name(name,year, cur)
+    cond = init_dico_cond()
+
+    print(json.dumps(recherche(cur, cond)))
